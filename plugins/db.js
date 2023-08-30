99e981345466262d430f07327dc70c890ee8e5b3
devin <77528948+zhengdechang@users.noreply.github.com>
2023-08-30 16:40:41 +0800

    Update db.js

--- plugins/db.js
+++ plugins/db.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     const mongoose = require("mongoose");
     mongoose.set('useFindAndModify', false);
-    mongoose.connect("mongodb+srv://dechang:dechang@cluster0.b9vurxx.mongodb.net/node-vue-moba?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb+srv://username:password@cluster0.b9vurxx.mongodb.net/node-vue-moba?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
