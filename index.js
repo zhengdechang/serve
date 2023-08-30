3440db65ddb5897affa7995d80bae1dc1b3fd7a5
zhengdechang <git config core.autocrlf false>
2023-08-30 14:21:19 +0800

    update

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const express = require("express")
+/*
+ * @Description: 
+ * @Author: Devin
+ * @Date: 2023-08-30 14:00:04
+ */
+const express = require("express");
 
-const app = express()
+const app = express();
 // const cors = require('cors')
 
-app.use(require('cors')())
+app.use(require("cors")());
 // app.use(cors({
 //     origin: 'http://127.0.0.1', //webpack打包后会生成index.html和static目录，我直接放在本地启动的nginx静态目录html下用来运行webpack打包文件，所以Origin地址为http://127.0.0.1
 //     allowedHeaders: 'Origin, x-requested-with, Content-Type, X-Token', //X-Token为自定义的头字段
 //     credentials: true //设置成true 请求中才会带上cookie信息，否则请求失败
 // }));
 
+app.use(express.json());
 
-app.use(express.json())
+app.set("secret", "sdasdsadsad");
 
-app.set('secret','sdasdsadsad')
+require("./router/admin")(app);
+require("./router/user")(app);
+require("./router/comment")(app);
+require("./router/liked")(app);
+require("./plugins/db")(app);
 
-require('./router/admin')(app)
-require('./router/user')(app)
-require('./router/comment')(app)
-require('./router/liked')(app)
-require('./plugins/db')(app)
+app.listen(3001, () => {
+  console.log("http://localhost:3001");
+});
 
-
-app.listen(3001,() =>{
-    console.log('http://localhost:3001')
-})
+module.exports = app;
